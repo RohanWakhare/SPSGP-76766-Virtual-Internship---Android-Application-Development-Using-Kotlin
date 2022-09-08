baebcbd081afb36f569c0a55839e9d5b90f1dd5e
Sunny Sahsi <sahsisunny@gmail.com>
2022-09-08 05:52:48 +0530

    remove all files for uploading main projects

--- Affirmations/app/src/main/java/com/sahsisunny/affirmations/model/Affirmations.kt
+++
@@ -1,9 +0,0 @@
-package com.sahsisunny.affirmations.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class Affirmations(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
-)
